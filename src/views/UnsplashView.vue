a7fcd8e9dade4d4ce32cbdfe6b8387d4cbea95b7
hmm365 <happyham52@gmail.com>
2022-11-08 16:51:19 +0900

    1108

---
+++ src/views/UnsplashView.vue
@@ -0,0 +1,25 @@
+<!-- @format -->
+
+<template>
+    <div>
+        <HeaderCont />
+        <TitleCont title="Unsplash" desc="referce api" />
+        <FooterCont />
+        <ContactCont />
+    </div>
+</template>
+
+<script>
+import HeaderCont from '@/components/HeaderCont.vue';
+import FooterCont from '@/components/FooterCont.vue';
+import TitleCont from '@/components/TitleCont.vue';
+import ContactCont from '@/components/ContactCont.vue';
+export default {
+    components: {
+        HeaderCont,
+        FooterCont,
+        TitleCont,
+        ContactCont,
+    },
+};
+</script>
